d6aa138860988b3673b2490ae1499a093333f848
Koo Bonwook <bnoo1333@gmail.com>
2022-01-15 23:36:49 +0900

    6챕터 챌린지 완료

--- 06-time-manipulation-operators/projects/challenge/Starter.playground/Contents.swift
+++ 06-time-manipulation-operators/projects/challenge/Starter.playground/Contents.swift
@@ -4,37 +4,23 @@ import Foundation
 // A subject you get values from
 let subject = PassthroughSubject<Int, Never>()
 
-<#Add your code here#>
+let solution = subject
+    .collect(.byTime(RunLoop.main, .seconds(0.5)))
+// 이상하게 DispatchQueue로 돌리면 is가 아니고 i\ns 가 됨
+//    .collect(.byTime(DispatchQueue.main, .seconds(0.5)))
+    .map {
+        String($0.map { Character(Unicode.Scalar($0)!)})
+    }
+
+let clap = subject
+    .debounce(for: .seconds(0.9), scheduler: DispatchQueue.main)
+    .map { _ in "👏" }
 
 // Let's roll!
 startFeeding(subject: subject)
 
-/*:
-Copyright (c) 2021 Razeware LLC
+let subecription = subject.sink { _ in }
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-distribute, sublicense, create a derivative work, and/or sell copies of the
-Software in any work that is designed, intended, or marketed for pedagogical or
-instructional purposes related to programming, coding, application development,
-or information technology.  Permission for such use, copying, modification,
-merger, publication, distribution, sublicensing, creation of derivative works,
-or sale is expressly withheld.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
+let subscription1 = Publishers.Merge(solution, clap).sink {
+    print($0)
+}
